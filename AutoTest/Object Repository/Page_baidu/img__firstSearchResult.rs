fb7fa064840af16981001ded6282dfabe284d62c
DongyuZhou-1018 <152261704+DongyuZhou-1018@users.noreply.github.com>
2023-12-01 13:04:54 +0800

    Add files via upload
    remove to new folder

---
+++ AutoTest/Object Repository/Page_baidu/img__firstSearchResult.rs
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>img__firstSearchResult</name>
+   <tag></tag>
+   <elementGuidId>f8ee08df-b9dd-405c-b252-dd24713d258d</elementGuidId>
+   <imagePath></imagePath>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;app&quot;)/div[@class=&quot;card-page&quot;]/div[@class=&quot;graph-result-card&quot;]/div[@class=&quot;graph-similar graph-container&quot;]/div[@class=&quot;graph-similar-list&quot;]/div[@class=&quot;general-waterfall&quot;]/div[@class=&quot;general-imgcol&quot;]/a[@class=&quot;general-imgcol-item&quot;]/span[@class=&quot;general-imgcol-item-bg graph-imgbg-fff&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='app']/div/div[3]/div/div[2]/div/div/a[@data-index=&quot;0&quot; and @class='general-imgcol-item']/img</value>
+      </entry>
+      <entry>
+         <key>IMAGE</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>img</value>
+      <webElementGuid>992fdf8f-9d41-4f12-bcb5-58d1936ee40f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-click</name>
+      <type>Main</type>
+      <value>{&quot;bt&quot;: &quot;b-img&quot;}</value>
+      <webElementGuid>36627fa6-a0af-46ad-b9cf-0b26182185fa</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>general-imgcol-item-bg graph-imgbg-fff</value>
+      <webElementGuid>a4226e85-e690-4a1e-8785-0092b09417ca</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;app&quot;)/div[@class=&quot;card-page&quot;]/div[@class=&quot;graph-result-card&quot;]/div[@class=&quot;graph-similar graph-container&quot;]/div[@class=&quot;graph-similar-list&quot;]/div[@class=&quot;general-waterfall&quot;]/div[@class=&quot;general-imgcol&quot;]/a[@class=&quot;general-imgcol-item&quot;]/span[@class=&quot;general-imgcol-item-bg graph-imgbg-fff&quot;]</value>
+      <webElementGuid>56315868-f539-4b45-98fb-38cd014d639a</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='app']/div/div[3]/div/div[2]/div/div/a[@data-index=&quot;0&quot; and @class='general-imgcol-item']/img</value>
+      <webElementGuid>f43561cc-4448-4704-952b-7c44354c1d8a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a[@data-index=&quot;0&quot; and @class='general-imgcol-item']/img</value>
+      <webElementGuid>b05b57ec-443b-461e-a33d-c99693cbdd4b</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ AutoTest/Object Repository/Page_baidu/span__general-imgcol-item-bg graph-imgbg-fff.rs
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span__general-imgcol-item-bg graph-imgbg-fff</name>
+   <tag></tag>
+   <elementGuidId>45070b54-d2d3-4467-ad39-f8e4b4e895a1</elementGuidId>
+   <imagePath>Screenshots/Targets/Page_/span__general-imgcol-item-bg graph-imgbg-fff.png</imagePath>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;app&quot;)/div[@class=&quot;card-page&quot;]/div[@class=&quot;graph-result-card&quot;]/div[@class=&quot;graph-similar graph-container&quot;]/div[@class=&quot;graph-similar-list&quot;]/div[@class=&quot;general-waterfall&quot;]/div[@class=&quot;general-imgcol&quot;]/a[@class=&quot;general-imgcol-item&quot;]/span[@class=&quot;general-imgcol-item-bg graph-imgbg-fff&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='app']/div/div[3]/div/div[2]/div/div/a[@data-index=&quot;${data-index}&quot; and @class='general-imgcol-item']/img</value>
+      </entry>
+      <entry>
+         <key>IMAGE</key>
+         <value>Screenshots/Targets/Page_/span__general-imgcol-item-bg graph-imgbg-fff.png</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>784bd691-bd36-4c89-8b8c-187ccba47008</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-click</name>
+      <type>Main</type>
+      <value>{&quot;bt&quot;: &quot;b-img&quot;}</value>
+      <webElementGuid>cbf5c7e5-758b-401e-b90d-94fecc3267bb</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>general-imgcol-item-bg graph-imgbg-fff</value>
+      <webElementGuid>2be0b577-7896-4c7e-8927-e9506a555f09</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;app&quot;)/div[@class=&quot;card-page&quot;]/div[@class=&quot;graph-result-card&quot;]/div[@class=&quot;graph-similar graph-container&quot;]/div[@class=&quot;graph-similar-list&quot;]/div[@class=&quot;general-waterfall&quot;]/div[@class=&quot;general-imgcol&quot;]/a[@class=&quot;general-imgcol-item&quot;]/span[@class=&quot;general-imgcol-item-bg graph-imgbg-fff&quot;]</value>
+      <webElementGuid>5de3cac5-f98e-4990-be66-556593134e21</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='app']/div/div[3]/div/div[2]/div/div/a[@data-index=&quot;${data-index}&quot; and @class='general-imgcol-item']/img</value>
+      <webElementGuid>620358e8-d869-4eca-b0a1-993d76871040</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a[@data-index=&quot;${data-index}&quot; and @class='general-imgcol-item']/img</value>
+      <webElementGuid>5949b630-64ae-4438-bdf9-5e1454266a3e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
